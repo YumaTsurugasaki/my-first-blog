11faf1bc76534b6bd4183e550af11f77ce68ea16
Yuma Tsurugasaki <a5816060@aoyama.jp>
2018-06-20 15:53:37 +0900

    My Django Girls app, first commit

--- pythontest/p9_3.py
+++ pythontest/p9_3.py
@@ -8,6 +8,11 @@ np.random.seed(19680801)
 
 matplotlib.rcParams['axes.unicode_minus'] = False
 fig, ax = plt.subplots()
+ax.plot(np.random.rand(20), '-o', ms=0, lw=0, alpha=0.7, mfc='orange')
 ax.plot(10*np.random.randn(100), 10*np.random.randn(100), 'o')
 ax.set_title('Using hyphen instead of Unicode minus')
+fig.text(0.90, 0.05, 'Unicode minus',
+         fontsize=50, color='gray',
+         ha='right', va='bottom', alpha=0.5)
+
 plt.show()
